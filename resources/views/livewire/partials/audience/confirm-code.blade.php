b690d73a3148c7fb0ab789282dc3b5040f612fc0
trungduc1294 <trungduc.1294@gmail.com>
2023-11-16 18:38:40 +0700

    update noti to wait 10s after payment succcess

--- resources/views/livewire/partials/audience/confirm-code.blade.php
+++ resources/views/livewire/partials/audience/confirm-code.blade.php
@@ -1,5 +1,6 @@
 <div class="verify_by_code loading-container">
     <h3>Check your email and enter the verification code.</h3>
+    <h4 style="color: red; margin-bottom: 10px;"><bold>!IMPORTANT: After successful payment, please wait for 10 seconds for the payment gateway to redirect you back to our ICHST website to complete the transaction (do not exit in the meantime).</bold></h4>
     <div class="verify_code_container">
         <input wire:model="user_code_input" type="text" placeholder="Enter the verification code here. Ex: abcXYz">
         <span>{{$errMessage}}</span>
--- resources/views/livewire/partials/author/checkcode.blade.php
+++ resources/views/livewire/partials/author/checkcode.blade.php
@@ -1,5 +1,6 @@
 <div class="check_code loading-container">
     <h3>Please enter the verification code sent to your email</h3>
+    <h4 style="color: red; margin-bottom: 10px;"><bold>!IMPORTANT: After successful payment, please wait for 10 seconds for the payment gateway to redirect you back to our ICHST website to complete the transaction (do not exit in the meantime).</bold></h4>
     <div class="code_input">
         <input type="text" wire:model="user_input_code" placeholder="Enter the verification code here, for example: abcXYZ.">
         <div class="error_box">
